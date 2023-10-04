c127057c95fa102b16fc771f8318ca8ec4d28e3e
achtu <oblakop9@gmail.com>
2023-10-04 15:48:44 +0200

    added -like- icon

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -3,13 +3,14 @@ import Card from 'react-bootstrap/Card';
 import Dropdown from 'react-bootstrap/Dropdown';
 import NavItem from 'react-bootstrap/NavItem';
 import NavLink from 'react-bootstrap/NavLink';
+import { FcLikePlaceholder } from "react-icons/fc";
 
 export default function ArtistCard({img, name}) {
  const [ events, setEvents] = useState({
   event: undefined
  })
   const app_id = "123";
-  async function fetchArtistData() {
+  async function fetchEventsData() {
     const baseUrl = "https://rest.bandsintown.com";
 
     const url = `${baseUrl}/artists/${name}/events?app_id=${encodeURIComponent(
@@ -17,36 +18,38 @@ export default function ArtistCard({img, name}) {
     )}&date=upcoming`;
 
     try {
+     
       const response = await fetch(url);
-      const artistData = await response.json();
+      const eventsData = await response.json();
 
       setEvents({
-        id: artistData.id
+        id: eventsData.id,
+        artist_id: eventsData.artist_id
       });
     } catch (error) {
-      // Handle network errors or other exceptions
+    
       console.error("Error fetching data:", error);
     }
   }
  
   const showEvents = () => {
 
-    fetchArtistData()
-    console.log(events.id)
+    fetchEventsData()
+   
   };
   return (
     <>
-        <Card style={{ width: '20rem', margin:"10px"}}>
+        <Card style={{ width: '20rem', margin:"10px", position:"relative"}}>
       <Card.Img variant="top" src={img} alt="maroon"/>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-     
+     <FcLikePlaceholder className='like-i'/>
       </Card.Body>
   
     <Dropdown as={NavItem}  onClick={showEvents}>
       <Dropdown.Toggle as={NavLink} >Upcoming events...</Dropdown.Toggle>
       <Dropdown.Menu>
-        <Dropdown.Item>Event 1 id: {events.id}</Dropdown.Item>
+        <Dropdown.Item>Event 1</Dropdown.Item>
         <Dropdown.Item>Event 2</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
