e9b6dd36ab8022aa40816f0a6d816dc12c9140f2
achtu <oblakop9@gmail.com>
2023-10-04 15:15:08 +0200

    added `showEvents` Button

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,10 +1,39 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import NavItem from 'react-bootstrap/NavItem';
+import NavLink from 'react-bootstrap/NavLink';
 
 export default function ArtistCard({img, name}) {
+ const [ events, setEvents] = useState({
+  event: undefined
+ })
+  const app_id = "123";
+  async function fetchArtistData() {
+    const baseUrl = "https://rest.bandsintown.com";
+
+    const url = `${baseUrl}/artists/${name}/events?app_id=${encodeURIComponent(
+      app_id
+    )}&date=upcoming`;
+
+    try {
+      const response = await fetch(url);
+      const artistData = await response.json();
+
+      setEvents({
+        id: artistData.id
+      });
+    } catch (error) {
+      // Handle network errors or other exceptions
+      console.error("Error fetching data:", error);
+    }
+  }
  
+  const showEvents = () => {
+
+    fetchArtistData()
+    console.log(events.id)
+  };
   return (
     <>
         <Card style={{ width: '20rem', margin:"10px"}}>
@@ -13,11 +42,14 @@ export default function ArtistCard({img, name}) {
         <Card.Title>{name}</Card.Title>
      
       </Card.Body>
-      <ListGroup variant="flush">
-      <ListGroup.Item>Event 1 </ListGroup.Item>
-      <ListGroup.Item>Event 2</ListGroup.Item>
-   
-    </ListGroup>
+  
+    <Dropdown as={NavItem}  onClick={showEvents}>
+      <Dropdown.Toggle as={NavLink} >Upcoming events...</Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item>Event 1 id: {events.id}</Dropdown.Item>
+        <Dropdown.Item>Event 2</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
     </Card>
 
 </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,56 +2,50 @@ import React, { useState } from "react";
 import ArtistCard from "../components/ArtistCard";
 import SearchEngine from "../components/SearchEngine";
 export default function Home() {
- const [artistName, setArtistName] = useState({
-  name: undefined
- })
-
+  const [artistName, setArtistName] = useState({
+    name: undefined,
+  });
 
   const app_id = "123";
-        async function fetchArtistData() {
-          const baseUrl = "https://rest.bandsintown.com";
-      
-          const url = `${baseUrl}/artists/${artistName}?app_id=${encodeURIComponent(app_id)}`;
-          
-          try {
-              const response = await fetch(url);
-              const artistData = await response.json();
-               
-              setArtistName({
-              
-                name: artistData.name,
-                img: artistData.image_url,
-               
-              })
-          } catch (error) {
-              // Handle network errors or other exceptions
-              console.error('Error fetching data:', error);
-          }
-      }
-      
-    
+  async function fetchArtistData() {
+    const baseUrl = "https://rest.bandsintown.com";
 
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        fetchArtistData();
-      };
-     
+    const url = `${baseUrl}/artists/${artistName}?app_id=${encodeURIComponent(
+      app_id
+    )}`;
+
+    try {
+      const response = await fetch(url);
+      const artistData = await response.json();
+
+      setArtistName({
+        name: artistData.name,
+        img: artistData.image_url,
+      });
+    } catch (error) {
+      // Handle network errors or other exceptions
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchArtistData();
+  };
 
   return (
-   <div  style={{textAlign:"center"}}>
-   <SearchEngine 
-      artistName={artistName}
-      handleSubmit={handleSubmit}
-      setArtistName={setArtistName} 
-   />
- 
-{artistName.name ? <ArtistCard img={artistName.img} name={artistName.name}/> : <p>Start searching</p>}
-  
+    <div style={{ textAlign: "center" }}>
+      <SearchEngine
+        artistName={artistName}
+        handleSubmit={handleSubmit}
+        setArtistName={setArtistName}
+      />
 
-   </div>
-  )
+      {artistName.name ? (
+        <ArtistCard img={artistName.img} name={artistName.name} />
+      ) : (
+        <p>Start searching</p>
+      )}
+    </div>
+  );
 }
-
-
-
-
